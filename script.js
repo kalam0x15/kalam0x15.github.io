d7b8bc7ec3adeeef483ced1f30534305a02f43d3
kalam0x15 <118707135+kalam0x15@users.noreply.github.com>
2023-03-22 11:32:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready (function(){
+    $('.python').on('click',function(){
+        window.open("https://github.com/kalam0x15/Python_projects")
+    })
+    $('.javascript').on('click',function(){
+        window.open("https://github.com/kalam0x15/JavaScript_project")
+    })
+    $('.html').on('click',function(){
+        window.open("https://github.com/kalam0x15")
+    })
+    $('.arduino').on('click',function(){
+        window.open("https://github.com/kalam0x15")
+    })
+})
